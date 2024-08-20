281841bc405b80a825235c29006a49502797f32d
Jorge <jorgevitorp9@gmail.com>
2024-08-20 10:03:24 -0300

    📦 refactor: deleted base structure

---
+++ src/app/+not-found.tsx
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+import { View } from "react-native";
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: "Oops!" }} />
+      <View>not found</View>
+    </>
+  );
+}
---
+++ src/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import "react-native-reanimated";
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(pages)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
+  );
+}
