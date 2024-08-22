0f7476d6052e82fd0617591a290fb2cdb1a27d8d
Jorge <jorgevitorp9@gmail.com>
2024-08-22 16:03:47 -0300

    ✨ feat: custom tab bar navigation

--- src/app/(pages)/_layout.tsx
+++ src/app/(pages)/_layout.tsx
@@ -1,15 +1,33 @@
+import { COLORS } from "@/constants/Colors";
 import { Tabs } from "expo-router";
 import React from "react";
 import Header from "../components/Header/Header";
+import TabBarButton from "../components/TabBarButton/TabBarButton";
 
 export default function TabLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 90,
+          backgroundColor: COLORS.secondaryBlack,
+          display: "flex",
+        },
+        tabBarItemStyle: {
+          width: 20,
+          marginHorizontal: 50,
+        },
+        lazy: true,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           header: () => <Header title="Home" backButton={true} />,
-          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarButton focused={focused} title="Home" />
+          ),
         }}
       />
     </Tabs>
---
+++ src/app/components/TabBarButton/TabBarButton.tsx
@@ -0,0 +1,43 @@
+import { COLORS } from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, Text, View } from "react-native";
+import { TabBarButtonProps } from "./TabBarButton.props";
+
+export default function TabBarButton({ focused, title }: TabBarButtonProps) {
+  return (
+    <View
+      style={{
+        ...styles.button,
+        backgroundColor: focused ? COLORS.black : "transparent",
+      }}
+    >
+      <Ionicons
+        name="library"
+        size={16}
+        color={focused ? COLORS.green : COLORS.white}
+      />
+      <Text
+        style={{ ...styles.text, color: focused ? COLORS.green : COLORS.white }}
+      >
+        Library
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    height: 70,
+    width: 70,
+    borderRadius: 50,
+  },
+  buttonSelected: {
+    backgroundColor: COLORS.black,
+  },
+  text: {
+    fontFamily: "LatoSemiBold",
+  },
+});
