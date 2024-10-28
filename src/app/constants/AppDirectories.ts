a56ef6187a9ea3f2979fbb8616fceaadb3d2d7e8
Jorge <jorgevitorp9@gmail.com>
2024-10-28 20:35:52 -0300

    ✨ feat(AppDirectories.ts): create the default directory if it does not exist

--- src/app/constants/AppDirectories.ts
+++ src/app/constants/AppDirectories.ts
@@ -2,5 +2,9 @@ import * as FileSystem from "expo-file-system";
 
 const APP_DIRECTORY = FileSystem.documentDirectory + "/music-player/";
 
+FileSystem.getInfoAsync(APP_DIRECTORY).then(({ exists }) => {
+  if (!exists) FileSystem.makeDirectoryAsync(APP_DIRECTORY);
+});
+
 export const DOWNLOAD_DIRECTORY = APP_DIRECTORY + "songs/";
 export const STORAGE_FILE = APP_DIRECTORY + "storage.json";
