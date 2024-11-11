1527c48f5600ab5b11344d7aef6d48feaab3730c
Jorge <jorgevitorp9@gmail.com>
2024-11-11 12:53:53 -0300

    ✨ feat(playlist.tsx): edit playlist and delete playlist

--- src/app/(pages)/library/playlist.tsx
+++ src/app/(pages)/library/playlist.tsx
@@ -1,10 +1,20 @@
+import Button from "@/components/Button/Button";
 import { COLORS } from "@/constants/Colors";
 import { Playlist } from "@/interfaces/Playlist";
 import { StorageContext } from "@/services/Storage/Storage.service";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
 import { useLocalSearchParams } from "expo-router";
-import { useContext } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { useContext, useState } from "react";
+import {
+  GestureResponderEvent,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { XStack, YStack } from "tamagui";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { FontAwesome5, MaterialIcons } from "@expo/vector-icons";
 
 export default function PlaylistPage() {
   const storageService = useContext(StorageContext);
@@ -28,15 +38,72 @@ export default function PlaylistPage() {
 
   const hasDescription = !!playlist.description;
 
+  const [optionsIsOpened, setOptionsIsOpened] = useState(false);
+
+  const toggleOptions = () => setOptionsIsOpened(!optionsIsOpened);
+
+  const onOptionsPress = (event: GestureResponderEvent) => {
+    event.stopPropagation();
+    toggleOptions();
+  };
+
   return (
     <View style={styles.view}>
-      <XStack>
+      <XStack width="100%">
         <Image source={imageSource} style={styles.image} resizeMode="stretch" />
         <YStack
           {...styles.informations}
           justifyContent={hasDescription ? "space-between" : "flex-start"}
         >
-          <Text style={styles.name}>{playlist.name}</Text>
+          <XStack
+            width="100%"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <Text style={styles.name}>{playlist.name}</Text>
+            <Button
+              buttonStyles={{ width: "auto", backgroundColor: "none" }}
+              icon={
+                <SimpleLineIcons
+                  name="options-vertical"
+                  size={22}
+                  color={COLORS.white}
+                  style={{ marginRight: 10 }}
+                />
+              }
+              onPress={onOptionsPress}
+            />
+
+            {optionsIsOpened && (
+              <YStack {...styles.playlistActions}>
+                <Button
+                  icon={
+                    <MaterialIcons
+                      name="close"
+                      size={22}
+                      color={COLORS.white}
+                    />
+                  }
+                  buttonStyles={styles.dialogCloseIcon}
+                  onPress={toggleOptions}
+                />
+                <Button
+                  title="Edit Details"
+                  icon={
+                    <Ionicons name="pencil" size={22} color={COLORS.white} />
+                  }
+                  buttonStyles={styles.actionsButton}
+                />
+                <Button
+                  title="Delete Playlist"
+                  icon={
+                    <FontAwesome5 name="trash" size={22} color={COLORS.red} />
+                  }
+                  buttonStyles={styles.actionsButton}
+                />
+              </YStack>
+            )}
+          </XStack>
 
           {playlist.description && (
             <Text
@@ -70,10 +137,11 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   image: {
-    width: 150,
+    width: "40%",
     height: 200,
   },
   informations: {
+    width: "60%",
     marginLeft: 10,
   },
   name: {
@@ -92,5 +160,32 @@ const styles = StyleSheet.create({
     color: COLORS.white,
     fontFamily: "LatoSemiBold",
     fontSize: 16,
+  },
+  playlistActions: {
+    position: "absolute",
+    top: 0,
+    right: "20%",
+    zIndex: 999,
+    backgroundColor: COLORS.secondaryBlack,
+    borderRadius: 5,
+    paddingTop: 30,
+    paddingBottom: 10,
+    paddingHorizontal: 20,
+  },
+  actionsButton: {
+    width: "auto",
+    backgroundColor: COLORS.black,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+  dialogCloseIcon: {
+    width: "auto",
+    backgroundColor: COLORS.transparentWhite,
+    paddingHorizontal: 3,
+    paddingVertical: 2,
+    position: "absolute",
+    top: 5,
+    right: 8,
   },
 });
