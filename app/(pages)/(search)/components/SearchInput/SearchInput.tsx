d930613300526718d49cc247762b9d13851b2d6c
Jorge <jorgevitorp9@gmail.com>
2024-11-27 12:49:51 -0300

    ✨ feat(SearchInput): added isInitialValue to validators of control

--- app/(pages)/(search)/components/SearchInput/SearchInput.tsx
+++ app/(pages)/(search)/components/SearchInput/SearchInput.tsx
@@ -1,19 +1,26 @@
 import TextInputControlled from "@/components/TextInputControlled/TextInputControlled";
 import { COLORS } from "@/constants/Colors";
 import { useFormControl } from "@/hooks/useFormControl/useFormControl";
+import { isInitialValue } from "@/validators/isInitialValue";
 import { required } from "@/validators/required";
 import { router } from "expo-router";
 import { StyleSheet, View } from "react-native";
 
 export function SearchInput({ defaultValue }: { defaultValue: string }) {
-  const control = useFormControl(defaultValue, [required]);
-  const { value } = control;
+  const control = useFormControl(defaultValue, [
+    required,
+    isInitialValue(defaultValue),
+  ]);
+  const { value, isValid } = control;
 
-  const goToResultsPage = () =>
+  const goToResultsPage = () => {
+    if (!isValid) return;
+
     router.push({
       pathname: "/results",
       params: { query: value },
     });
+  };
 
   return (
     <View style={{ ...styles.alignInCenter, backgroundColor: COLORS.black }}>
