3b690c07ae31c5f08769b34a2e3fb748db618bca
Jorge <jorgevitorp9@gmail.com>
2024-12-13 12:15:01 -0300

    ✨ feat(useCleanPageOnInactive): hook to clear the page when the screen is inactive and when it is active it renders again

---
+++ app/hooks/useCleanPageOnInactive/useCleanPageOnInactive.ts
@@ -0,0 +1,15 @@
+import { usePathname } from "expo-router";
+import { useEffect, useState } from "react";
+
+export function useCleanPageOnInactive(page: React.JSX.Element, path: string) {
+  const [screen, setScreen] = useState<React.JSX.Element | null>(page);
+  const currentPath = usePathname();
+
+  useEffect(() => {
+    const screen = currentPath === path ? page : null;
+
+    setScreen(screen);
+  }, [currentPath]);
+
+  return screen;
+}
