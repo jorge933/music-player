a183eb9747224dc266668fc27ab9455872d56a87
Jorge <jorgevitorp9@gmail.com>
2024-12-15 22:36:04 -0300

    📦 refactor(AddSongDialog): using useCallback to optimize the performance of component functions

--- app/features/Library/pages/Playlist/components/AddSongDialog/AddSongDialog.tsx
+++ app/features/Library/pages/Playlist/components/AddSongDialog/AddSongDialog.tsx
@@ -6,7 +6,7 @@ import { useStorage } from "@/hooks/useStorage/useStorage";
 import { Playlist } from "@/interfaces/Playlist";
 import { Song } from "@/interfaces/Song";
 import { MaterialIcons } from "@expo/vector-icons";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { ScrollView, StyleSheet } from "react-native";
 import { YGroup } from "tamagui";
 import { AddSongDialogProps } from "./AddSongDialog.types";
@@ -18,12 +18,12 @@ export function AddSongDialog({
 }: AddSongDialogProps) {
   const storage = useStorage();
 
-  const getItem = (key: string) => {
+  const getItem = useCallback((key: string) => {
     const itemInStorage = storage.getItem<string>(key);
     const item = JSON.parse(itemInStorage);
 
     return item;
-  };
+  }, []);
 
   const allSongs: Song[] = getItem("songs");
   const [allPlaylists, setAllPlaylists] = useState<Playlist[]>(
@@ -32,40 +32,46 @@ export function AddSongDialog({
 
   const currentPlaylist = allPlaylists.find((item) => item.id === playlistId);
 
-  const updatePlaylistSongs = (songId: string) => {
-    const alreadyAdded = currentPlaylist?.songs.includes(songId);
+  const updatePlaylistSongs = useCallback(
+    (songId: string) => {
+      const alreadyAdded = currentPlaylist?.songs.includes(songId);
 
-    const updatedPlaylists = allPlaylists.map((playlist) => {
-      if (playlist.id !== playlistId) return playlist;
+      const updatedPlaylists = allPlaylists.map((playlist) => {
+        if (playlist.id !== playlistId) return playlist;
 
-      const updatedSongs = !alreadyAdded
-        ? [...playlist.songs, songId]
-        : playlist.songs.filter((id) => id !== songId);
+        const updatedSongs = !alreadyAdded
+          ? [...playlist.songs, songId]
+          : playlist.songs.filter((id) => id !== songId);
 
-      return { ...playlist, songs: updatedSongs };
-    });
+        return { ...playlist, songs: updatedSongs };
+      });
 
-    setAllPlaylists(updatedPlaylists);
+      setAllPlaylists(updatedPlaylists);
 
-    const playlistsSerialized = JSON.stringify(updatedPlaylists);
+      const playlistsSerialized = JSON.stringify(updatedPlaylists);
 
-    storage.setItem("playlists", playlistsSerialized);
-  };
+      storage.setItem("playlists", playlistsSerialized);
+    },
+    [allPlaylists, playlistId, storage],
+  );
 
-  const generateActionButton = (songId: string) => {
-    const alreadyAdded = currentPlaylist?.songs.includes(songId);
+  const generateActionButton = useCallback(
+    (songId: string) => {
+      const alreadyAdded = currentPlaylist?.songs.includes(songId);
 
-    const iconName = alreadyAdded ? "playlist-add-check" : "playlist-add";
-    const buttonColor = alreadyAdded ? COLORS.green : COLORS.white;
+      const iconName = alreadyAdded ? "playlist-add-check" : "playlist-add";
+      const buttonColor = alreadyAdded ? COLORS.green : COLORS.white;
 
-    return (
-      <Button
-        icon={<MaterialIcons name={iconName} size={22} color={buttonColor} />}
-        onPress={() => updatePlaylistSongs(songId)}
-        buttonStyles={styles.actionButton}
-      />
-    );
-  };
+      return (
+        <Button
+          icon={<MaterialIcons name={iconName} size={22} color={buttonColor} />}
+          onPress={() => updatePlaylistSongs(songId)}
+          buttonStyles={styles.actionButton}
+        />
+      );
+    },
+    [currentPlaylist, updatePlaylistSongs],
+  );
 
   return (
     <BaseDialog
