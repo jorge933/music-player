fc631c7a5a8e5d3e18a741ab5c345df445bd1079
Jorge <jorgevitorp9@gmail.com>
2024-12-16 20:55:51 -0300

    ✨ feat(useFetch): default timeout as 10 seconds

--- app/hooks/useFetch/useFetch.ts
+++ app/hooks/useFetch/useFetch.ts
@@ -1,11 +1,13 @@
 import axios, { AxiosRequestConfig } from "axios";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { UseFetchReturn } from "./useFetch.types";
 
 export function useFetch<T>(options: AxiosRequestConfig): UseFetchReturn<T> {
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState<unknown>();
   const [isFetching, setIsFetching] = useState(true);
+
+  if (!options.timeout) options.timeout = 10000;
 
   const fetchData = useCallback(() => {
     setIsFetching(true);
@@ -21,6 +23,8 @@ export function useFetch<T>(options: AxiosRequestConfig): UseFetchReturn<T> {
       .catch(setError)
       .finally(() => setIsFetching(false));
   }, [options]);
+
+  useEffect(() => fetchData(), []);
 
   return { data, error, isFetching, fetchData };
 }
