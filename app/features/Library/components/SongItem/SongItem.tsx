d86ca92a9a419e700a231ffe84bcaf41c19cac82
Jorge <jorgevitorp9@gmail.com>
2024-12-21 12:13:27 -0300

    📦 refactor(SongItem): formatting duration

--- app/features/Library/components/SongItem/SongItem.tsx
+++ app/features/Library/components/SongItem/SongItem.tsx
@@ -4,9 +4,13 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import { StyleSheet, Text, View } from "react-native";
 import { XStack, YStack } from "tamagui";
 import { SongItemProps } from "./SongItem.types";
+import { useMemo } from "react";
+import { formatSecondsToTime } from "@/helpers/formatSecondsToTime";
 
 export function SongItem({ song, actionButton }: SongItemProps) {
   const { title, duration } = song;
+  const formattedDuration = useMemo(() => formatSecondsToTime(duration), []);
+
   return (
     <View style={{ ...ITEM_STYLES.item, ...styles.item }}>
       <XStack alignItems="center">
@@ -24,7 +28,7 @@ export function SongItem({ song, actionButton }: SongItemProps) {
             ellipsizeMode="tail"
             style={ITEM_STYLES.subInformation}
           >
-            {duration}
+            {formattedDuration}
           </Text>
         </YStack>
       </XStack>
