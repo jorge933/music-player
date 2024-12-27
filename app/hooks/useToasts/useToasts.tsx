23f70bf6c1f18e84380b9729572bc2ff21c96e31
Jorge <jorgevitorp9@gmail.com>
2024-12-27 20:55:26 -0300

    📦 refactor(useToasts): addToast with useCallback

--- app/hooks/useToasts/useToasts.tsx
+++ app/hooks/useToasts/useToasts.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Toast } from "@/components/Toast/Toast";
 import { CreateToastFn } from "./useToast.types";
 
 export function useToasts() {
   const [toasts, setToasts] = useState<React.JSX.Element[]>([]);
 
-  const addToast: CreateToastFn = (message, type, duration) => {
+  const addToast = useCallback<CreateToastFn>((message, type, duration) => {
     const toastKey = Date.now().toString();
     const $toast = <Toast message={message} type={type} key={toastKey} />;
 
@@ -16,7 +16,7 @@ export function useToasts() {
         prevToasts.filter((currentToast) => currentToast.key !== toastKey),
       );
     }, duration);
-  };
+  }, []);
 
   return {
     toasts,
