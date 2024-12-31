6b050a309ef26fd81420e6b9e0e2893d1514ef26
Jorge <jorgevitorp9@gmail.com>
2024-12-31 10:44:48 -0300

    📦 refactor(DownloadSongProvider): removed useEffect

--- app/components/DownloadSongProvider/DownloadSongProvider.tsx
+++ app/components/DownloadSongProvider/DownloadSongProvider.tsx
@@ -1,17 +1,13 @@
 import { DownloadSongContext } from "@/contexts/downloadContext/downloadContext";
 import { DownloadItem } from "@/contexts/downloadContext/downloadContext.types";
-import { SongService } from "@/services/songService/songService";
-import { ReactNode, useEffect, useState } from "react";
-import { VideoDetails } from "../DownloadDialog/DownloadDialog.types";
 import { downloadSong } from "@/hooks/useDownloadManager/useDownloadManager";
+import { SongService } from "@/services/songService/songService";
+import { ReactNode, useState } from "react";
+import { VideoDetails } from "../DownloadDialog/DownloadDialog.types";
 
 export function DownloadSongProvider({ children }: { children: ReactNode }) {
   const [queue, setQueue] = useState<DownloadItem[]>([]);
   const songService = new SongService();
-
-  useEffect(() => {
-    console.log(queue);
-  }, [queue]);
 
   return (
     <DownloadSongContext.Provider
