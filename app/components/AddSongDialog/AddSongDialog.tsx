83fd3a4411f92eb5130657799a38658ec6d1e604
Jorge <jorgevitorp9@gmail.com>
2025-01-17 13:01:43 -0300

    📦 refactor(AddSongDialog): removed unused import

--- app/components/AddSongDialog/AddSongDialog.tsx
+++ app/components/AddSongDialog/AddSongDialog.tsx
@@ -2,7 +2,6 @@ import { BaseDialog } from "@/components/BaseDialog/BaseDialog";
 import { Button } from "@/components/Button/Button";
 import { SongItem } from "@/components/SongItem/SongItem";
 import { COLORS } from "@/constants/Colors";
-import { useStorage } from "@/hooks/useStorage/useStorage";
 import { Playlist } from "@/interfaces/Playlist";
 import { MaterialIcons } from "@expo/vector-icons";
 import React, { useCallback, useMemo, useState } from "react";
