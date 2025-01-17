9c6eeab5a4495b726a55756b26334985377575ad
Jorge <jorgevitorp9@gmail.com>
2025-01-17 18:28:13 -0300

    🧪 test(useCleanPageOnInactive): added tests to hook cases

---
+++ app/hooks/useCleanPageOnInactive/useCleanPageOnInactive.spec.tsx
@@ -0,0 +1,59 @@
+import { renderHook, waitFor } from "testUtils";
+import { usePathname } from "expo-router";
+import { useCleanPageOnInactive } from "./useCleanPageOnInactive";
+import { Text } from "react-native";
+import { act } from "react";
+
+jest.mock("expo-router", () => ({
+  usePathname: jest.fn(),
+}));
+
+describe("useCleanPageOnInactive", () => {
+  it("should display the page if the current path matches", () => {
+    (usePathname as jest.Mock).mockReturnValue("/home");
+
+    const page = <Text>Page</Text>;
+
+    const {
+      result: { current },
+    } = renderHook(() => useCleanPageOnInactive(page, "/home"));
+
+    expect(current).toBe(page);
+  });
+
+  it("should return null if the current path does not match", () => {
+    (usePathname as jest.Mock).mockReturnValue("/about");
+
+    const page = <Text>Page</Text>;
+
+    const {
+      result: { current },
+    } = renderHook(() => useCleanPageOnInactive(page, "/home"));
+
+    expect(current).toBeNull();
+  });
+
+  it("should update return state if path change", async () => {
+    const mockUsePathname = jest.spyOn(require("expo-router"), "usePathname");
+    mockUsePathname.mockReturnValue("/home");
+
+    const page = <Text>Page</Text>;
+
+    const { result, rerender } = renderHook(
+      ({ page, path }) => useCleanPageOnInactive(page, path),
+      {
+        initialProps: { page, path: "/home" },
+      },
+    );
+
+    expect(result.current).toBe(page);
+
+    act(() => {
+      mockUsePathname.mockReturnValue("/about");
+    });
+
+    rerender({ page, path: "/home" });
+
+    expect(result.current).toBeNull();
+  });
+});
