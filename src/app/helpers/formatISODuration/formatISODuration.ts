d33ec7f9638987a9f96d4e5e3042e7d264d8e6b5
Jorge <jorgevitorp9@gmail.com>
2025-02-05 15:56:30 -0300

    📦 refactor(formatISODuration): return 0 if dont have matches

--- src/app/helpers/formatISODuration/formatISODuration.ts
+++ src/app/helpers/formatISODuration/formatISODuration.ts
@@ -1,11 +1,15 @@
 export function formatISODuration(duration: string): number {
   const regex = /PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/;
+  const matches = duration.match(regex) as RegExpMatchArray;
+
+  if (!matches) return 0;
+
   const [
     ,
     hoursInString = "0",
     minutesInStringInString = "0",
     secondsInString = "0",
-  ] = duration.match(regex) as RegExpMatchArray;
+  ] = matches;
 
   const hours = parseInt(hoursInString, 10);
   const minutes = parseInt(minutesInStringInString, 10);
