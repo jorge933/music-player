0e71af121f201bc93d22054665d6be32e5ccd6bd
Jorge <jorgevitorp9@gmail.com>
2025-02-06 11:01:22 -0300

    ✨ feat(useSongPlayerControl): skip to next song when current song playing finish

--- src/app/hooks/useSongPlayerControl/useSongPlayerControl.ts
+++ src/app/hooks/useSongPlayerControl/useSongPlayerControl.ts
@@ -1,4 +1,6 @@
+import { Playlist } from "@/interfaces";
 import { Song } from "@/interfaces/Song";
+import { PlaylistService } from "@/services/playlistService";
 import { SongService } from "@/services/songService";
 import { AVPlaybackStatus, Audio } from "expo-av";
 import { useState } from "react";
@@ -11,9 +13,9 @@ export type PlayingSong = {
 
 export class SongPlayerControl {
   private readonly songService = new SongService();
+  private readonly playlistService = new PlaylistService();
 
   private readonly playerState = useState<Audio.Sound | null>(null);
-  private readonly player = this.playerState[0];
   private readonly setPlayer = this.playerState[1];
 
   private readonly currentSongPlayingState = useState<PlayingSong | null>(null);
@@ -21,36 +23,85 @@ export class SongPlayerControl {
   private readonly setCurrentSongPlaying = this.currentSongPlayingState[1];
 
   async play(videoId: string, playlistId?: number) {
-    if (this.player) await this.player.unloadAsync();
-
     const player = new Audio.Sound();
 
-    this.setPlayer(player);
+    this.setPlayer((prev) => {
+      if (prev) prev.unloadAsync();
 
-    player.setOnPlaybackStatusUpdate(this.handleStatusUpdate);
+      return player;
+    });
 
     const song = this.songService.getById(videoId) as Song;
 
-    this.setCurrentSongPlaying({
+    const currentSongPlaying: PlayingSong = {
       song,
       playedSeconds: 0,
       playlistId: playlistId ?? 0,
-    });
+    };
 
-    player.loadAsync({ uri: song.path }, { shouldPlay: true });
+    player.setOnPlaybackStatusUpdate((status) =>
+      this.handleStatusUpdate.apply(this, [status, currentSongPlaying]),
+    );
+
+    this.setCurrentSongPlaying(currentSongPlaying);
+
+    player.loadAsync(
+      { uri: song.path },
+      { shouldPlay: true, positionMillis: (song.duration - 1) * 1000 },
+    );
   }
 
-  private handleStatusUpdate = (status: AVPlaybackStatus) => {
+  private handleStatusUpdate = (
+    status: AVPlaybackStatus,
+    currentSongPlaying: PlayingSong,
+  ) => {
     if (!status.isLoaded) return;
 
-    this.setCurrentSongPlaying(
-      (prev) =>
-        ({
-          ...prev,
-          playedSeconds: status.positionMillis / 1000,
-        }) as PlayingSong,
-    );
+    const { positionMillis } = status;
+    const playedSeconds = positionMillis / 1000;
+
+    this.setCurrentSongPlaying({
+      ...currentSongPlaying,
+      playedSeconds,
+    });
+
+    const roundedPlayedSeconds = Math.ceil(playedSeconds);
+    if (roundedPlayedSeconds >= currentSongPlaying.song.duration)
+      this.skipToNext(currentSongPlaying);
   };
+
+  private skipToNext(currentSongPlaying: PlayingSong) {
+    const { song, playlistId } = currentSongPlaying;
+
+    const nextSongId = this.getNextSongId(song.id, playlistId);
+
+    this.play(nextSongId, playlistId);
+  }
+
+  private getNextSongId(songId: string, playlistId: number) {
+    const songs = playlistId
+      ? (this.playlistService.getById(playlistId) as Playlist).songs
+      : this.songService.getAll();
+
+    const nextSongId = songs.reduce((value: string | null, current, index) => {
+      if (value) return value;
+
+      const isString = typeof current === "string";
+      const currentId = isString ? current : current.id;
+
+      if (currentId === songId) {
+        const nextItem = songs[index + 1] || songs[0];
+        const isString = typeof nextItem === "string";
+        const nextId = isString ? nextItem : nextItem.id;
+
+        return nextId;
+      }
+
+      return null;
+    }, null);
+
+    return nextSongId as string;
+  }
 }
 
 export function useSongPlayerControl() {
