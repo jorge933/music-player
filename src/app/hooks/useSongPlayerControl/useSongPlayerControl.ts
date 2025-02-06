d67ad789693a83b4c2dba2ab69ab11278c100f1a
Jorge <jorgevitorp9@gmail.com>
2025-02-06 15:51:50 -0300

    📦 refactor(useSongPlayerControl): using functions instead a class

--- src/app/hooks/useSongPlayerControl/useSongPlayerControl.ts
+++ src/app/hooks/useSongPlayerControl/useSongPlayerControl.ts
@@ -3,115 +3,132 @@ import { Song } from "@/interfaces/Song";
 import { PlaylistService } from "@/services/playlistService";
 import { SongService } from "@/services/songService";
 import { AVPlaybackStatus, Audio } from "expo-av";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export type PlayingSong = {
+export interface PlayingSong {
   song: Song;
   playlistId: number;
   playedSeconds: number;
-};
+}
 
-export class SongPlayerControl {
-  private readonly songService = new SongService();
-  private readonly playlistService = new PlaylistService();
-
-  private readonly playerState = useState<Audio.Sound | null>(null);
-  private readonly setPlayer = this.playerState[1];
-
-  private readonly currentSongPlayingState = useState<PlayingSong | null>(null);
-  readonly currentSongPlaying = this.currentSongPlayingState[0];
-  private readonly setCurrentSongPlaying = this.currentSongPlayingState[1];
-  private _songPlaying: PlayingSong | null = null;
-
-  private get songPlaying() {
-    return this._songPlaying;
-  }
-
-  private set songPlaying(song: PlayingSong | null) {
-    this._songPlaying = song;
-    this.setCurrentSongPlaying(song);
-  }
-
-  async play(videoId: string, playlistId?: number) {
-    const player = new Audio.Sound();
-
-    this.setPlayer((prev) => {
-      if (prev) prev.unloadAsync();
-
-      return player;
-    });
-
-    const song = this.songService.getById(videoId) as Song;
-
-    const currentSongPlaying: PlayingSong = {
-      song,
-      playedSeconds: 0,
-      playlistId: playlistId ?? 0,
-    };
-
-    player.setOnPlaybackStatusUpdate((status) =>
-      this.handleStatusUpdate.apply(this, [status]),
-    );
-
-    this.songPlaying = currentSongPlaying;
-
-    player.loadAsync({ uri: song.path }, { shouldPlay: true });
-  }
-
-  private handleStatusUpdate = (status: AVPlaybackStatus) => {
-    if (!status.isLoaded) return;
-
-    const { positionMillis } = status;
-    const playedSeconds = positionMillis / 1000;
-
-    this.songPlaying = {
-      ...this.songPlaying,
-      playedSeconds,
-    } as PlayingSong;
-
-    const roundedPlayedSeconds = Math.ceil(playedSeconds);
-    if (roundedPlayedSeconds >= this.songPlaying.song.duration)
-      this.skipToNext();
-  };
-
-  skipToNext() {
-    if (!this.songPlaying) return;
-
-    const { song, playlistId } = this.songPlaying;
-
-    const nextSongId = this.getNextSongId(song.id, playlistId);
-
-    this.play(nextSongId, playlistId);
-  }
-
-  private getNextSongId(songId: string, playlistId: number) {
-    const songs = playlistId
-      ? (this.playlistService.getById(playlistId) as Playlist).songs
-      : this.songService.getAll();
-
-    const nextSongId = songs.reduce((value: string | null, current, index) => {
-      if (value) return value;
-
-      const isString = typeof current === "string";
-      const currentId = isString ? current : current.id;
-
-      if (currentId === songId) {
-        const nextItem = songs[index + 1] || songs[0];
-        const isString = typeof nextItem === "string";
-        const nextId = isString ? nextItem : nextItem.id;
-
-        return nextId;
-      }
-
-      return null;
-    }, null);
-
-    return nextSongId as string;
-  }
+interface EssentialProps {
+  player: React.MutableRefObject<Audio.Sound | null>;
+  currentSongPlaying: PlayingSong | null;
+  setCurrentSongPlaying: React.Dispatch<
+    React.SetStateAction<PlayingSong | null>
+  >;
+  playlistService: PlaylistService;
+  songService: SongService;
 }
 
 export function useSongPlayerControl() {
-  const player = new SongPlayerControl();
+  const songService = new SongService();
+  const playlistService = new PlaylistService();
+  const player = useRef<Audio.Sound | null>(null);
+  const [currentSongPlaying, setCurrentSongPlaying] =
+    useState<PlayingSong | null>(null);
 
-  return player;
+  const generateEssentialPropsObj = () => {
+    return {
+      setCurrentSongPlaying,
+      player,
+      songService,
+      playlistService,
+      currentSongPlaying,
+    };
+  };
+
+  return {
+    play: (videoId: string, playlistId: number) =>
+      play(videoId, playlistId, generateEssentialPropsObj()),
+    currentSongPlaying,
+    skipToNext: () => skipToNext(generateEssentialPropsObj()),
+  };
+}
+
+async function play(
+  videoId: string,
+  playlistId: number,
+  essentialProps: EssentialProps,
+) {
+  const { player, setCurrentSongPlaying, songService } = essentialProps;
+  if (player.current) await player.current.unloadAsync();
+
+  player.current = new Audio.Sound();
+
+  const song = songService.getById(videoId) as Song;
+
+  player.current.loadAsync({ uri: song.path }, { shouldPlay: true });
+
+  player.current.setOnPlaybackStatusUpdate((status) =>
+    handleStatusUpdate(status, essentialProps),
+  );
+
+  setCurrentSongPlaying({ playedSeconds: 0, playlistId, song });
+}
+
+async function handleStatusUpdate(
+  status: AVPlaybackStatus,
+  essentialProps: EssentialProps,
+) {
+  const { setCurrentSongPlaying } = essentialProps;
+  if (!status.isLoaded) return;
+
+  const playedSeconds = status.positionMillis / 1000;
+
+  setCurrentSongPlaying((currentSongPlaying) => {
+    const newValue = {
+      ...(currentSongPlaying as PlayingSong),
+      playedSeconds,
+    };
+
+    const roundedPlayedSeconds = Math.ceil(playedSeconds);
+    const songEnded = roundedPlayedSeconds >= newValue.song.duration;
+
+    if (songEnded)
+      skipToNext({ ...essentialProps, currentSongPlaying: newValue });
+
+    return newValue;
+  });
+}
+
+function skipToNext(essentialProps: EssentialProps) {
+  const { currentSongPlaying } = essentialProps;
+  if (!currentSongPlaying) return;
+
+  const { song, playlistId } = currentSongPlaying;
+
+  const nextSongId = getNextSongId(song.id, playlistId, essentialProps);
+
+  play(nextSongId, playlistId, essentialProps);
+}
+
+function getNextSongId(
+  songId: string,
+  playlistId: number,
+  essentialProps: EssentialProps,
+) {
+  const { playlistService, songService } = essentialProps;
+  const songs = playlistId
+    ? (playlistService.getById(playlistId) as Playlist).songs
+    : songService.getAll();
+
+  const nextSongId = songs.reduce((value: string | null, current, index) => {
+    if (value) return value;
+
+    const isString = typeof current === "string";
+    const currentId = isString ? current : current.id;
+
+    if (currentId === songId) {
+      const nextItem = songs[index + 1] || songs[0];
+      const isString = typeof nextItem === "string";
+      const nextId = isString ? nextItem : nextItem.id;
+
+      return nextId;
+    }
+
+    return null;
+  }, null);
+
+  return nextSongId as string;
 }
