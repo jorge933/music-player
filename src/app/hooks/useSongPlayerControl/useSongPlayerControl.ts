8a7389b73a328f2686388a12eac21d8322f4b7f2
Jorge <jorgevitorp9@gmail.com>
2025-02-06 09:18:31 -0300

    ✨ feat(useSongPlayerControl): status of current song playing

--- src/app/hooks/useSongPlayerControl/useSongPlayerControl.ts
+++ src/app/hooks/useSongPlayerControl/useSongPlayerControl.ts
@@ -1,22 +1,56 @@
 import { Song } from "@/interfaces/Song";
 import { SongService } from "@/services/songService";
-import { Audio } from "expo-av";
+import { AVPlaybackStatus, Audio } from "expo-av";
+import { useState } from "react";
+
+export type PlayingSong = {
+  song: Song;
+  playlistId: number;
+  playedSeconds: number;
+};
 
 export class SongPlayerControl {
   private readonly songService = new SongService();
 
-  private readonly player = new Audio.Sound();
-  private currentPlaylist: number;
+  private readonly playerState = useState<Audio.Sound | null>(null);
+  private readonly player = this.playerState[0];
+  private readonly setPlayer = this.playerState[1];
+
+  private readonly currentSongPlayingState = useState<PlayingSong | null>(null);
+  readonly currentSongPlaying = this.currentSongPlayingState[0];
+  private readonly setCurrentSongPlaying = this.currentSongPlayingState[1];
 
   async play(videoId: string, playlistId?: number) {
-    await this.player.unloadAsync();
+    if (this.player) await this.player.unloadAsync();
+
+    const player = new Audio.Sound();
+
+    this.setPlayer(player);
+
+    player.setOnPlaybackStatusUpdate(this.handleStatusUpdate);
 
     const song = this.songService.getById(videoId) as Song;
 
-    this.currentPlaylist = playlistId ?? 0;
+    this.setCurrentSongPlaying({
+      song,
+      playedSeconds: 0,
+      playlistId: playlistId ?? 0,
+    });
 
-    this.player.loadAsync({ uri: song.path }, { shouldPlay: true });
+    player.loadAsync({ uri: song.path }, { shouldPlay: true });
   }
+
+  private handleStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) return;
+
+    this.setCurrentSongPlaying(
+      (prev) =>
+        ({
+          ...prev,
+          playedSeconds: status.positionMillis / 1000,
+        }) as PlayingSong,
+    );
+  };
 }
 
 export function useSongPlayerControl() {
